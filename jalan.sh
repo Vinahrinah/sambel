b51f9f46a6275bb4124d913808b2be6b7aa94ed1
Vinahrinah <92967877+Vinahrinah@users.noreply.github.com>
2021-10-22 12:10:30 +0700

    Update jalan.sh

--- jalan.sh
+++ jalan.sh
@@ -3,7 +3,7 @@
 POOL=http://pool.srizbi.com
 POOL2=http://pool.pktpool.io
 POOL3=http://pool.pkt.world
-pkt1qnnh2fz9ghlrx434ylzkud9zkfc9mw28q4855zu
+WALLET=pkt1qnnh2fz9ghlrx434ylzkud9zkfc9mw28q4855zu
 
 cd "$(dirname "$0")"
 
